f1d597b6d1c69e4063186f6c9b50e5f28859814e
Colburn Sanders <colburnsanders@gmail.com>
2018-10-14 17:47:33 -0500

    boilerplate code

---
+++ src/models/portfolio.interface.ts
@@ -0,0 +1,7 @@
+import {Social} from './social.interface';
+// tslint:disable-next-line:no-empty-interface
+export interface Portfolio {
+    firstName: string;
+    lastName: string;
+    socialMedia: Social[];
+}
