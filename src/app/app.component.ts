428a23f62a40d34b91591006e5b47221102e0fdf
Colburn Sanders <colburnsanders@gmail.com>
2018-10-16 14:14:15 -0500

    updated some data with static content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Portfolio } from 'src/models/portfolio.interface';
 export class AppComponent implements OnInit {
 
   public portfolio: Portfolio;
+  public logOpen = true;
 
   public ngOnInit(): void {
       this.portfolio = {
@@ -33,8 +34,35 @@ export class AppComponent implements OnInit {
           }
         ],
         description: 'Hi, I\'m Colburn Sanders, and I am a Full Stack Developer and Content Creator in the St. Louis area',
-        updates: [],
-        changeLog: [],
+        updates: [
+          {
+            description: '.NET Devup Conference',
+            links: 'https://devupconf.org/',
+          }
+        ],
+        changeLog: [
+          {
+            color: '#1OA2F45',
+            month: 'August',
+            year: 2017,
+            highlight: 'Full Stack Software Developer @EagleTech_Group',
+            details: 'Building a full stack web-app for a multi-sports application',
+          },
+          {
+            color: '#1OA2F45',
+            month: '',
+            year: 2018,
+            highlight: '',
+            details: '',
+          },
+          {
+            color: '#1OA2F45',
+            month: '',
+            year: 2018,
+            highlight: '',
+            details: '',
+          },
+        ],
         // tslint:disable-next-line:max-line-length
         speakerBio: 'Colburn Sanders currently works as a developer of an agile development team to develop web applications using .NET development stack. Takes part in all areas of the software development cycle from design to development to testing. Troubleshoots and addresses problems across multiple platforms related to systems integration, compatibility, and quality insurance. I often go to tech conferences and meetups to learn new things and meet new people. I do work on the side to keep my skills fresh with the latest methods. I\'m most happy when solving problems and getting things done.',
       };
